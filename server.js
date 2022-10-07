17dde2542bdbc92cc1d6a35048ea34f187eda13e
CasperHogstrom <casperhogstrom3@gmail.com>
2022-10-07 13:48:28 +0200

    Node now working and the server is up

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const server = express();
 
 const fs = require('fs');
+server.use(express.static('public'));
 
-server.get('/', (req, res) = () => {
+server.get('/', (req, res) => {
 
-    fs.readFile('/public/index.html', (err, data) => {
+    fs.readFile('index.html', (err, data) => {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
     });
 
 });
+
+server.listen(8080);
